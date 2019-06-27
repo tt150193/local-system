64d0d6b1da081467e93536f5f93cd80f79f2d28e
Tal Tal <tt150193@gmail.com>
2019-06-27 10:07:32 +0700

    VER-0.2.3

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,9 +1,3 @@
 sudo apt update -y && sudo apt upgrade -y
 git clone https://github.com/creationix/nvm.git ~/.nvm
 sudo echo "source ~/.nvm/nvm.sh" >> ~/.bashrc && sudo echo "source ~/.nvm/nvm.sh" >> ~/.profile
-nvm install 9.0.0
-nvm use 9.0.0
-npm install
-
-echo "SETUP SUCCESSFULL"
-echo "RUN WITH node main.js"
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 git clone https://github.com/tt150193/local-system.git
 cd local-system
 sh INSTALL.sh
+nvm install 9.0.0
+nvm use 9.0.0
+
+node main.js
