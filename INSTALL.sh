8a636c153fed532e38e1d17aa98403467314d046
Tal Tal <tt150193@gmail.com>
2019-06-27 10:04:24 +0700

    VER-0.2.2

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,3 +4,6 @@ sudo echo "source ~/.nvm/nvm.sh" >> ~/.bashrc && sudo echo "source ~/.nvm/nvm.sh
 nvm install 9.0.0
 nvm use 9.0.0
 npm install
+
+echo "SETUP SUCCESSFULL"
+echo "RUN WITH node main.js"
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #CRETA => LOCAL-DEMO
+
+git clone https://github.com/tt150193/local-system.git
+cd local-system
+sh INSTALL.sh
